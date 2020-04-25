77e1408671f85671b222f6c129d8b4418ee4c1b3
Alex Zhd <my_email@domain.com>
2020-04-25 22:39:12 +0700

    small refactoring

--- src/main/java/com/example/simple_twitter/repositories/MessageRepository.java
+++ src/main/java/com/example/simple_twitter/repositories/MessageRepository.java
@@ -1,12 +1,11 @@
-package com.example.simple_twitter.repos;
+package com.example.simple_twitter.repositories;
 
 import com.example.simple_twitter.domain.Message;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 
 // Create the Repository
-public interface MessageRepo extends CrudRepository<Message, Long> {
+public interface MessageRepository extends CrudRepository<Message, Long> {
 
   List<Message> findByTag(String tag); // поиск в БД по тэгу
 }
