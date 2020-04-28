41de55986631112dca74858ccc3241c9930bc590
Alex Zhd <my_email@domain.com>
2020-04-28 20:57:23 +0700

    Загрузка файлов на сервер и раздача статики

--- src/main/java/com/example/simple_twitter/config/MvcConfig.java
+++ src/main/java/com/example/simple_twitter/config/MvcConfig.java
@@ -1,17 +1,30 @@
 package com.example.simple_twitter.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
+  @Value("${upload.path}")
+  private String uploadPath;
+
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
-//    registry.addViewController("/home").setViewName("home");
-//    registry.addViewController("/").setViewName("home");
-//    registry.addViewController("/hello").setViewName("hello");
     registry.addViewController("/login").setViewName("login");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/img/**")
+        .addResourceLocations("file://" + uploadPath + "/");
+
+    registry
+        .addResourceHandler("/static/**")
+        .addResourceLocations("classpath:/static/");
   }
 }
