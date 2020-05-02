706150f0219fba6e010ee9ef94646a9ca579fc69
Alex Zhd <my_email@domain.com>
2020-05-02 21:32:32 +0700

    Аннотация @OneToMany. Редактирование сообщений пользователя

--- src/main/java/com/example/simple_twitter/domain/User.java
+++ src/main/java/com/example/simple_twitter/domain/User.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -29,11 +30,27 @@ public class User implements UserDetails {
   @Enumerated(EnumType.STRING)
   private Set<Role> roles;
 
+  @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private Set<Message> messages;
+
   @Email(message = "Email is not correct")
   @NotBlank(message = "Email cannot be empty")
   private String email;
 
   private String activationCode;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return Objects.equals(id, user.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 
   public boolean isAdmin() {
     return roles.contains(Role.ADMIN);
@@ -118,5 +135,13 @@ public class User implements UserDetails {
 
   public void setActivationCode(String activationCode) {
     this.activationCode = activationCode;
+  }
+
+  public Set<Message> getMessages() {
+    return messages;
+  }
+
+  public void setMessages(Set<Message> messages) {
+    this.messages = messages;
   }
 }
